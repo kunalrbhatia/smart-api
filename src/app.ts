369ac792d9675e5268ea7de4302d24f294a58363
Kunal R. Bhatia <kunal.bhatia@quincus.com>
2023-07-16 03:53:34 +0530

    added getNextExpiry function

--- src/app.ts
+++ src/app.ts
@@ -27,7 +27,40 @@ let bnNextFutureLTP: string = '';
 const smart_api = new SmartAPI({
   api_key: API_KEY,
 });
+const getNextExpiry = () => {
+  const today = new Date();
+  const dayOfWeek = today.getDay();
+  const daysUntilThursday = 4 - dayOfWeek;
+  const comingThursday = new Date(
+    today.getFullYear(),
+    today.getMonth(),
+    today.getDate() + daysUntilThursday
+  );
+  // Get the year, month, and day of the coming Thursday
+  const year = comingThursday.getFullYear();
+  const month = comingThursday.getMonth() + 1;
+  const day = comingThursday.getDate();
 
+  // Format the date as ddmmmyyyy
+  const months = [
+    'JAN',
+    'FEB',
+    'MAR',
+    'APR',
+    'MAY',
+    'JUN',
+    'JUL',
+    'AUG',
+    'SEP',
+    'OCT',
+    'NOV',
+    'DEC',
+  ];
+  const monthName = months[month - 1];
+  const formattedDate = `${day}${monthName}${year}`;
+
+  return formattedDate;
+};
 const doOrder = () => {
   smart_api
     .generateSession(CLIENT_CODE, CLIENT_PASSWORD)
