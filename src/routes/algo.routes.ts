42db2a8438e4d1050bc422cca49cee21565e5108
Kunal Rajan Bhatia <47558199+kunalrbhatia@users.noreply.github.com>
2025-09-04 16:35:47 +0530

    Code modularization (#14)
    * ✨ feat(server): restructure server and routes, add error handling and configuration
    
    * 🐛 fix(algo): removebruno files
    
    * ✨ feat(api): add api routes for fetching VIX data and improve error handling

---
+++ src/routes/algo.routes.ts
@@ -0,0 +1,32 @@
+import { Router, Request, Response } from 'express';
+import { checkMarketConditionsAndExecuteTrade } from '../helpers/apiService';
+import { setCred } from '../helpers/functions';
+import { ALGO } from '../helpers/constants';
+
+const router = Router();
+
+router.post('/run-short-straddle', async (req: Request, res: Response) => {
+  console.log(`\n${ALGO}: ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^`);
+  try {
+    const istTz = new Date().toLocaleString('default', { timeZone: 'Asia/Kolkata' });
+    console.log(`${ALGO}: time, ${istTz}`);
+
+    setCred(req);
+
+    const lots: number = req.body.lots;
+    const lossPerLot: number = req.body.loss_per_lot;
+
+    console.log(`${ALGO}: lots: ${lots}`);
+
+    const response = await checkMarketConditionsAndExecuteTrade(lots, lossPerLot);
+    console.log(`${ALGO} response: ${response}`);
+
+    res.json({ response });
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ response: err });
+  }
+  console.log(`${ALGO}: -----------------------------------`);
+});
+
+export default router;
