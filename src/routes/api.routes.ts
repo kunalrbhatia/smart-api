5c0fa47b0c6b8e21a2fb57e2ec033c101d064722
Kunal Bhatia <kunal.bhatia@hotmail.com>
2025-09-09 12:00:17 +0530

    ✨ feat(api routes): added routes to fetch indices data

--- src/routes/api.routes.ts
+++ src/routes/api.routes.ts
@@ -2,13 +2,16 @@ import { Router, Request, Response } from 'express';
 import { getIndexScrip, getLtpData } from '../helpers/apiService';
 import { setCred } from '../helpers/functions';
 import { ALGO } from '../helpers/constants';
+import { INDICES } from 'krb-smart-api-module';
 
 const router = Router();
 
 router.post('/getVix', async (req: Request, res: Response) => {
   console.log(`\n${ALGO}: ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^`);
   try {
-    const istTz = new Date().toLocaleString('default', { timeZone: 'Asia/Kolkata' });
+    const istTz = new Date().toLocaleString('default', {
+      timeZone: 'Asia/Kolkata',
+    });
     console.log(`${ALGO}: time, ${istTz}`);
     setCred(req);
     const indiaVix = await getIndexScrip({ scriptName: 'INDIA VIX' });
@@ -24,4 +27,68 @@ router.post('/getVix', async (req: Request, res: Response) => {
   }
   console.log(`${ALGO}: -----------------------------------`);
 });
+router.post('/getNifty', async (req: Request, res: Response) => {
+  console.log(`\n${ALGO}: ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^`);
+  try {
+    const istTz = new Date().toLocaleString('default', {
+      timeZone: 'Asia/Kolkata',
+    });
+    console.log(`${ALGO}: time, ${istTz}`);
+    setCred(req);
+    const data = await getIndexScrip({ scriptName: INDICES.NIFTY });
+    const ltpData = await getLtpData({
+      exchange: data[0].exch_seg,
+      symboltoken: data[0].token,
+      tradingsymbol: data[0].symbol,
+    });
+    res.status(200).json({ data: ltpData });
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ response: err });
+  }
+  console.log(`${ALGO}: -----------------------------------`);
+});
+router.post('/getBankNifty', async (req: Request, res: Response) => {
+  console.log(`\n${ALGO}: ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^`);
+  try {
+    const istTz = new Date().toLocaleString('default', {
+      timeZone: 'Asia/Kolkata',
+    });
+    console.log(`${ALGO}: time, ${istTz}`);
+    setCred(req);
+    const data = await getIndexScrip({ scriptName: INDICES.BANKNIFTY });
+    const ltpData = await getLtpData({
+      exchange: data[0].exch_seg,
+      symboltoken: data[0].token,
+      tradingsymbol: data[0].symbol,
+    });
+    res.status(200).json({ data: ltpData });
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ response: err });
+  }
+  console.log(`${ALGO}: -----------------------------------`);
+});
+router.post('/getSensex', async (req: Request, res: Response) => {
+  console.log(`\n${ALGO}: ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^`);
+  try {
+    const istTz = new Date().toLocaleString('default', {
+      timeZone: 'Asia/Kolkata',
+    });
+    console.log(`${ALGO}: time, ${istTz}`);
+    setCred(req);
+    const data = await getIndexScrip({ scriptName: INDICES.SENSEX });
+    const ltpData = await getLtpData({
+      exchange: data[0].exch_seg,
+      symboltoken: data[0].token,
+      tradingsymbol: data[0].symbol,
+    });
+    res.status(200).json({ data: ltpData });
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ response: err });
+  }
+  console.log(`${ALGO}: -----------------------------------`);
+});
+
 export default router;
