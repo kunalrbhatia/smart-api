20bcd9ac3aeab773e5449cd2d42592cdf635737d
Kunal Bhatia <kunal.bhatia@hotmail.com>
2026-08-04 12:52:09 +0530

    fix: remove unused import in sessionStore test

--- __tests__/store/sessionStore.test.ts
+++ __tests__/store/sessionStore.test.ts
@@ -2,7 +2,6 @@ import fs from 'fs';
 import path from 'path';
 import {
   getSessionState,
-  saveSessionState,
   setStraddleOpenedToday,
   setMtmBaseline,
 } from '../../src/store/sessionStore';
